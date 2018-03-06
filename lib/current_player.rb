6dcbb6617ebf40b71df7720e75677f5897a43e2e
SDS 800 <jeremyhyman@gmail.com>
2018-03-06 16:56:45 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,9 +10,9 @@ board.each do |token|
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0 
-    puts "X"
+  if turn_count(board) % 2 == 0
+    return "X"
   else
-    puts "O"
+    return "O"
   end
 end
